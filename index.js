05ff2c258a9d1f458ddd536b3620206b7e7c6e79
admin <karthickdurai587@gmail.com>
2022-03-22 16:14:09 +0530

    all

--- index.js
+++ index.js
@@ -1,56 +1,60 @@
-const express = require('express');
-const { MongoClient } = require("mongodb");
-const app = express();
-const recordRoutes = express.Router();
-// Replace the uri string with your MongoDB deployment's connection string.
-const uri = "mongodb+srv://karthickraja:Ben123456789@cluster0.h7hwq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri);
+// const express = require('express');
+// const { MongoClient } = require("mongodb");
+// const app = express();
+// const recordRoutes = express.Router();
+// // Replace the uri string with your MongoDB deployment's connection string.
+// const uri = "mongodb+srv://karthickraja:Ben123456789@cluster0.h7hwq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri);
+// var bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+// app.get('/api/customer/list', (req, res) => {
+//   async function run() {
+//     try {
+//       await client.connect();
+//       const database = client.db('Restarents');
+//       const movies = database.collection('customers');
+//       // Query for a movie that has the title 'Back to the Future'
+//       // const query = { Username: 'Username' };
+//       const movie = await movies.find({}).toArray()
+//       res.send(movie);
+//       // console.log(movie)
+//     } finally {
+//       // Ensures that the client will close when you finish/error
+//       await client.close();
+//     }
+//   }
+//   run().catch(console.dir);
+// });
+// app.post('/api/customer/new', (req, res)=> {
+  
+//   async function run() {
+    
+//     try {
+//       await client.connect();
+//       const database = client.db("Restarents");
+//       const haiku = database.collection("customers");
+//       // create a document to insert
+//       var doc = {
+//         Username:req.body.Username,
+//         Password: req.body.Password,
+//         Email: req.body.Email,
+//         Phonenumber: req.body.Phonenumber,
+//         CreatedOn: req.body.CreatedOn,
+//         CreatedBy: req.body.CreatedBy
+//       }
+//       const result = await haiku.insertOne(doc);
+//       res.send({ statusCode: 200, message: `A document was inserted with the _id: ${result.insertedId}` });
+//     } finally {
+//       await client.close();
+//     }
+//   }
+//   run().catch(console.dir);
+// });
 
-app.get('/api/customer/list', (req, res) => {
-  async function run() {
-    try {
-      await client.connect();
-      const database = client.db('Restarents');
-      const movies = database.collection('customers');
-      // Query for a movie that has the title 'Back to the Future'
-      // const query = { Username: 'Username' };
-      const movie = await movies.find({}).toArray()
-      res.send(movie);
-      // console.log(movie)
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
-    }
-  }
-  run().catch(console.dir);
-});
-app.post('/api/customer/new',function(req,res){
-  async function run() {
-    try {
-      await client.connect();
-      const database = client.db("Restarents");
-      const haiku = database.collection("customers");
-      // create a document to insert
-      const doc = {
-        Username: req.Username,
-        Password: req.Password,
-        Email: req.Email,
-        Phonenumber: req.Phonenumber,
-        CreatedOn: req.Phonenumber,
-        CreatedBy: req.CreatedBy
-      }
-      const result = await haiku.insertOne(doc);
-      res.send({statusCode:200,message : `A document was inserted with the _id: ${result.insertedId}`});
-    } finally {
-      await client.close();
-    }
-  }
-  run().catch(console.dir);
-});
 
 
-
-app.listen(4000, () => console.log("Server Running"))
+// app.listen(4000, () => console.log("Server Running"))
 
 
 
@@ -85,47 +89,56 @@ app.listen(4000, () => console.log("Server Running"))
 //     console.log(movie);
 
 // app.listen(3005,()=> console.log("Server Running"))
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+var monk = require('monk');
+const router = express.Router();
+const app = express();
+var db = monk("mongodb+srv://karthickraja:Ben123456789@cluster0.h7hwq.mongodb.net/Restarents?retryWrites=true&w=majority",);
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.post('/adduser', function(req, res) {
+    var Username = req.body.Username;
+    var Password = req.body.Password;
+    var Email = req.body.Email;
+    var Phonenumber = req.body.Phonenumber;
+    var CreatedOn = req.body.CreatedOn;
+    var CreatedBy = req.body.CreatedBy;
+    var collection = db.get('customers');
+    collection.insert({
+        Username: Username,
+        Password: Password,
+        Email:Email,
+        Phonenumber:Phonenumber,
+        CreatedOn:CreatedOn,
+        CreatedBy:CreatedBy
+    }, function (err, result) {
+        if (err) {
+          res.send({ statusCode: 400, message: "There was a problem adding the information to the database." });
+            res.send();
+        }
+        else {
+          res.send({ statusCode: 200, message: `A document was inserted with the _id: ${result.insertedId}` });
+            // res.json(doc);
+        }
+    });
+});
 
 
-
-
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// var mongo = require('mongodb');
-// var monk = require('monk');
-// const router = express.Router();
-// const app = express();
-// var db = monk("mongodb+srv://karthickraja:Ben123456789@cluster0.h7hwq.mongodb.net/Restarents?retryWrites=true&w=majority",);
-
-// app.post('/adduser', function(req, res) {
-
-//     var Username = req.body.Username;
-//     var Password = req.body.Password;
-//     var Email = req.body.Email;
-//     var Phonenumber = req.body.Phonenumber;
-//     var CreatedOn = req.body.CreatedOn;
-//     var CreatedBy = req.body.CreatedBy;
-
-//     var collection = db.get('customers');
-
-//     collection.insert({
-
-//         Username: Username,
-//         Password: Password,
-//         Email:Email,
-//         Phonenumber:Phonenumber,
-//         CreatedOn:CreatedOn,
-//         CreatedBy:CreatedBy
-//     }, function (err, doc) {
-//         if (err) {
-//             res.send("There was a problem adding the information to the database.");
-//         }
-//         else {
-//             res.json(req.body); // <- here you could return the new object instead.
-//             // res.json(doc);
-//         }
-//     });
-// });
-
-
-// app.listen(4000, () => console.log("Server Running"))
+app.get('/api/customer/list', function(req, res) {
+  var collection = db.get('customers');
+  collection.find({}, function (err, result) {
+      if (err) {
+        res.send({ statusCode: 400, message: "There was a problem adding the information to the database." });
+         
+      }
+      else {
+        res.send(result);
+          // res.json(doc);
+      }
+  })
+});
+app.listen(4000, () => console.log("Server Running"))
